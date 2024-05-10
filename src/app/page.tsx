61804413afe062602b16f5f0016a075581c9402f
ljain07 <lavisha.jain.neuromonk@gmail.com>
2024-05-10 19:53:58 +0530

    working on design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import MyCarousel from "./component/crunchy";
 import { Suspense } from 'react'
 
+
+
+
 export default function Home() {
   return (
     <>
